ea6cc3fc94118afdf462d5d3551b113f9c6aeb6e
Mohammad Jazim <51312049+jazim5@users.noreply.github.com>
2025-07-05 02:03:26 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBK7njURm4slibrlQFjPoQZmxUPAYhsljw",
-  authDomain: "icdx-71147.firebaseapp.com",
-  projectId: "icdx-71147",
-  storageBucket: "icdx-71147.firebasestorage.app",
-  messagingSenderId: "676375930908",
-  appId: "1:676375930908:web:aa9252a2f3f7fd085657d9",
-  measurementId: "G-593GQ4C5SK"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "1::web:",
+  measurementId: ""
 };
 
 // Initialize Firebase
